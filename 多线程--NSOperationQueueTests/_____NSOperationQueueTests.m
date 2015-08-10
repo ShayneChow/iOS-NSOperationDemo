3e87f42aef0b23437ffec7a935ea1aa10b6d39df
Xiang <Xiang@XiangdeMacBook-Pro.local>
2015-08-10 19:59:05 +0800

    创建NSOperationQueue Target

---
+++ 多线程--NSOperationQueueTests/_____NSOperationQueueTests.m
@@ -0,0 +1,40 @@
+//
+//  _____NSOperationQueueTests.m
+//  多线程--NSOperationQueueTests
+//
+//  Created by Xiang on 15/8/10.
+//  Copyright (c) 2015年 周想. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface _____NSOperationQueueTests : XCTestCase
+
+@end
+
+@implementation _____NSOperationQueueTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

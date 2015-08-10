5004aa2c960440f487210f0f945063082564ddb7
Xiang <Xiang@XiangdeMBP.lan>
2015-08-11 02:03:58 +0800

    maxConcurrentOperationCount最大并发数

--- 多线程--NSOperationQueue/ViewController.m
+++ 多线程--NSOperationQueue/ViewController.m
@@ -11,11 +11,60 @@
 
 @interface ViewController ()
 
+/** 队列 */
+@property (nonatomic, strong) NSOperationQueue *queue;
+
 @end
 
 @implementation ViewController
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    // 1.创建队列
+    /*
+     GCD中有哪些队列:
+     并发: 自己创建, 全局
+     串行: 自己创建, 主队列
+     
+     NSOperationQueue:
+     主队列: mainQueue
+     自己创建: 会在子线程中执行
+     */
+    
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    // 自己创建的队列默认是并发, 如果设置maxConcurrentOperationCount = 1,就是串行
+    // 注意: 不能设置为0, 如果设置为0就不行执行任务\
+    //       默认情况下maxConcurrentOperationCount = -1
+    //       在开发中并发数最多尽量不要超过5~6条
+    queue.maxConcurrentOperationCount = 1;
+    
+    // 2.创建任务
+    [queue addOperationWithBlock:^{
+        [NSThread sleepForTimeInterval:0.1];
+        NSLog(@"1 == %@", [NSThread currentThread]);
+    }];
+    [queue addOperationWithBlock:^{
+        [NSThread sleepForTimeInterval:0.1];
+        NSLog(@"2 == %@", [NSThread currentThread]);
+    }];
+    [queue addOperationWithBlock:^{
+        [NSThread sleepForTimeInterval:0.1];
+        NSLog(@"3 == %@", [NSThread currentThread]);
+    }];
+    [queue addOperationWithBlock:^{
+        [NSThread sleepForTimeInterval:0.1];
+        NSLog(@"4 == %@", [NSThread currentThread]);
+    }];
+    [queue addOperationWithBlock:^{
+        [NSThread sleepForTimeInterval:0.1];
+        NSLog(@"5 == %@", [NSThread currentThread]);
+    }];
+    [queue addOperationWithBlock:^{
+        [NSThread sleepForTimeInterval:0.1];
+        NSLog(@"6 == %@", [NSThread currentThread]);
+    }];
+}
+
+- (void)operationQueue {
     // 1.创建队列
     NSOperationQueue *queue = [[NSOperationQueue alloc] init];
     // 2.创建任务
